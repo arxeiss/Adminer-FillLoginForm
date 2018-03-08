f09d4f5c00211394f8079c3a6c668b51b40d5a91
Pavel Kutáč <pavel.kutac@gmail.com>
2018-03-08 23:09:02 +0100

    Fix according to CSP

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Plugin to Adminer for fill login form by set values
 How to install plugins to Adminer: http://www.adminer.org/plugins/
 
 About plugin on http://www.kutac.cz/blog/weby-a-vse-okolo/adminer-plugin-fill-login-form/
+
+## Changelog
+8.3.2018 - Fix JS according to CSP (Content Security Policy). Added in Adminer [4.4.0 (released 2018-01-17)](https://github.com/vrana/adminer/blob/master/changes.txt)
--- fill-login-form.php
+++ fill-login-form.php
@@ -42,28 +42,28 @@ class FillLoginForm {
     
   public function loginForm(){  
     if(empty($_GET[DRIVER]) && empty($_GET["username"]) && empty($_GET["db"]) ){?>
-      <script>
+      <script<?php echo nonce(); ?>>
       document.addEventListener("DOMContentLoaded", function(event) {
-        var dr = document.querySelector("option[value='<?php echo $this->system; ?>']");
+        var dr = qs("option[value='<?php echo $this->system; ?>']");
         if(dr){ dr.selected = true; }
          
         <?php if(!empty($this->server)){ ?>
-          var s = document.querySelector("input[name='auth[server]']");
+          var s = qs("input[name='auth[server]']");
           if(s && s.value.trim() == ""){ s.value = "<?php echo $this->server ?>"; }
         <?php }
           
         if(!empty($this->name)){ ?>
-          var l = document.querySelector("input[name='auth[username]']");
+          var l = qs("input[name='auth[username]']");
           if(l && l.value.trim() == ""){ l.value = "<?php echo $this->name ?>"; }
         <?php }
         
         if(!empty($this->pass)){ ?>
-          var p = document.querySelector("input[name='auth[password]']");
+          var p = qs("input[name='auth[password]']");
           if(p && p.value.trim() == ""){ p.value = "<?php echo $this->pass ?>"; }
         <?php }
         
         if(!empty($this->database)){ ?>
-          var d = document.querySelector("input[name='auth[db]']");
+          var d = qs("input[name='auth[db]']");
           if(d && d.value.trim() == ""){ d.value = "<?php echo $this->database ?>"; }
         <?php } ?>
       });
